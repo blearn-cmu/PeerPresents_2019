9edcd3f00a7b58b7fc05bd35e189f8227d25fc45
blearn-cmu <blearn@andrew.cmu.edu>
2020-01-28 01:06:55 +0000

    started worker manager w/ request-promise-native

--- backend/app/managers/workerManager.js
+++ backend/app/managers/workerManager.js
@@ -1,97 +1,20 @@
+const request = require('request-promise-native')
 
+async function somefunc (ctx, next){
+    var options = {
+        uri: "test",
+        json: true
+    };
+    request(options)
+        .then(function (arg){
+            console.log('test');
+        })
+
+        .catch(function (err){
+            console.log(err);
+        });
+}
 
 module.exports = {
-    identity: function handle_identity_request(params) {
-        return new Promise((resolve, reject) => {
-            /* TODO */
-            // POST /id/login
-            // params: user, password
-
-            // POST /id/logout
-            // params: none
-
-            // PUT /id/user
-            // params: user, password
-
-            // GET /id/{user_id}
-            // params: none
-
-            // DELETE /id/{user_id}
-            // params: none
-        })
-    },
-
-    presentation: function handle_presentation_request(params) {
-        return new Promise((resolve, reject) => {
-            /* TODO */
-            // PUT /presentation/{id}/content/question
-            // params: question_text
-
-            // DELETE /presentation/{id}/content/question
-            // params: question_id
-
-            // PUT /presentation/{id}/org/{course|session|presentation}
-            // params: model
-
-            // DELETE /presentation/{id}/org/{course|session|presentation}
-            // params: model_id
-
-        })
-    },
-
-    study: function handle_study_request(params) {
-        return new Promise((resolve, reject) => {
-            /* TODO */
-            // PUT /study/study
-            // params: ? study_params
-
-            // DELETE /study/study/{study_id}
-            // params: none
-
-            // PUT /study/{study_id}/{presentation_id|course_id|user_id}
-            // params: none
-
-            // DELETE /study/{study_id}/{presentation_id|course_id|user_id}
-            // params: none
-        })
-    },
-
-    live: function handle_live_request(params) {
-        return new Promise((resolve, reject) => {
-            /* TODO */
-            // PUT /live/{presentation_id}
-            // params: owner_auth
-
-            // DELETE /live/{presentation_id}
-            // params: owner_auth
-
-            // GET /live{presentation_id}
-            // params: none
-        })
-    },
-
-    audience: function handle_audience_request(params) {
-        return new Promise((resolve, reject) => {
-            /* TODO */
-            // PUT /audience/{upvote|react|star|tag}
-            // params: presentation_id, parent_id
-
-            // DELETE /audience/{upvote|react|star|tag}
-            // params: presentation_id, parent_id
-
-            // PUT /audience/response
-            // params: presentation_id, question_id, response_text
-        })
-    },
-
-    research: function handle_research_request(params) {
-        return new Promise((resolve, reject) => {
-            /* TODO */
-            // GET /study/{study_id}/{model_id}
-            // params: none
-
-            // GET /study/{study_id}/all
-            // params: none
-        })
-    }
-};
+    somefunc
+}
